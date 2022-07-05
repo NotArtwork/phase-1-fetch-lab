3a98bc239df0d301b2473d44db26ea6afe536f91
NotArtwork <AntArce3@gmail.com>
2022-07-05 14:24:34 -0400

    commit tree ef45636a0ab7931d4fd11e15c0fcc079b6614ba1

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-function fetchBooks() {
+const fetchBooks = async () => {
   // To pass the tests, don't forget to return your fetch!
-  
+  let request = await fetch('https://anapioficeandfire.com/api/books') // Request data from Server/API and returns a response object.
+  let response = await request.json() // Returns data provided by server as an Object
+  renderBooks(response)
+  return fetch('https://anapioficeandfire.com/api/books')
+  // const main = document.querySelector('main')
+  //     response.forEach(book => { //
+  //     let h2 = document.createElement('h2')
+  //     h2.textContent = book.name;
+  //     main.appendChild(h2);
+  // })
 }
+
+// function fetchBooks() {
+//   return fetch("https://anapioficeandfire.com/api/books")
+//     .then(response => response.json())
+//     .then(json => renderBooks(json))
+// }
 
 function renderBooks(books) {
   const main = document.querySelector('main');
